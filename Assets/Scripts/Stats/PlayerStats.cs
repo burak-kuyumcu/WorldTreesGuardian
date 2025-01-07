63b12f5f81b9b5518fea04ec5c4651d6bc4fca9d
Aydın Burak Kuyumcu <burak_kuyumcu@yahoo.com.tr>
2025-01-08 00:37:06 +0300

    game over testi

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : CharacterStats
 {
     public Image healthBar;
+    public GameObject gameOverPanel; // Game Over panelini referans ekle
 
+
+    public override void Start()
+    {
+        base.Start();
+        gameOverPanel.SetActive(false); // Baþlangýçta gizli yap
+    }
     private void Update()
     {
         healthBar.fillAmount = currentHealth / maxHealth.GetValue();
     }
+
     public override void Die()
     {
-       
+        base.Die();
+        GameOver();
+    }
+
+    private void GameOver()
+    {
+        gameOverPanel.SetActive(true); // Game Over panelini aç
+        Time.timeScale = 0f; // Oyunu durdur
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Oyunu yeniden baþlat
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // Mevcut sahneyi yeniden yükle
     }
 }
